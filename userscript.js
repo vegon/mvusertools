e815de9df09302fc1344f72fdd2310225a15cc06
Marcelino Concepción <marcelinocb@gmail.com>
2012-10-21 13:53:16 +0100

    Actualizado jQuery a la versión 1.8.2

--- userscript.js
+++ userscript.js
@@ -8,7 +8,7 @@
 
 function addJQuery(callback) {
   var script = document.createElement("script");
-  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js");
   script.addEventListener('load', function() {
     var script = document.createElement("script");
     script.textContent = "(" + callback.toString() + ")();";
