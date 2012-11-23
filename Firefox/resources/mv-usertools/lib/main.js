6055280278061754d439de7edfca752ed1e2ea36
Jose Manuel Vega <jm.vega.farina@gmail.com>
2012-11-23 23:10:02 +0100

    añadida sin minified el plugin de la botonera

--- Firefox/resources/mv-usertools/lib/main.js
+++ Firefox/resources/mv-usertools/lib/main.js
@@ -5,5 +5,6 @@ pageMod.PageMod({
   contentScriptWhen: 'end',
   contentScriptFile: [data.url("jquery-1.8.2.min.js"),
                       data.url("tinycon.min.js"),
+					  data.url("jquery.a-tools-1.5.2.js"),
                       data.url("mvusertools.user.js")]
 });
